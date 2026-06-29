51917986918724df9085077cbc20b4845d2ceac4
Android Studio <devandroid@ort>
2026-06-29 09:46:43 -0300

    ultimos cambios

--- src/repository/publicacionRepository.js
+++ src/repository/publicacionRepository.js
@@ -17,7 +17,7 @@ export default class publicacionRepository {
      getAll = async () => {
         let returnArray = null;
         try {
-            const sql = `SELECT * FROM provincias`;
+            const sql = `SELECT * FROM publicacion`;
             const result = await this.getDBPool().query(sql);
             returnArray = result.rows;
         } catch (error) {
@@ -29,7 +29,7 @@ export default class publicacionRepository {
     getByIdAsync = async (id) => {
         let returnEntity = null;
         try {
-            const sql = `SELECT * FROM provincias WHERE id=$1`;
+            const sql = `SELECT * FROM publicacion WHERE usuarioId=$1`;
             const values = [id];
             const result = await this.getDBPool().query(sql, values);
             if (result.rows.length > 0){
@@ -44,7 +44,7 @@ export default class publicacionRepository {
     deleteByIdAsync = async (id) => {
         let returnRows = null;
          try {
-            const sql = `DELETE FROM provincias WHERE id=$1`;
+            const sql = `DELETE FROM publicacion WHERE id=$1`;
             const values = [id];
             const result = await this.getDBPool().query(sql, values);
             returnRows = result.rowCount;
@@ -54,34 +54,34 @@ export default class publicacionRepository {
         return returnRows;
     }
 
-     updateAsync = async (entity) => {
-        let returnRows = null;
-         try {
-            let id = entity.id;
-            const previousEntity = await this.getByIdAsync(id);
-            if (previousEntity == null) return 0;
-            const sql = `UPDATE provincias SET
-                        name              = $2,
-                        latitude            = $3,
-                        longitude            = $4,
-                        fullname    = $5,
-                        displayorder       = $6
-                    WHERE id = $1`;
-            const values = [
-            id,
-            entity?.name           ?? previousEntity?.name,
-            entity?.latitude         ?? previousEntity?.latitude,
-            entity?.longitude         ?? previousEntity?.longitude,
-            entity?.fullname ?? previousEntity?.fullname,
-            entity?.displayorder    ?? previousEntity?.displayorder
-        ];
-            const result = await this.getDBPool().query(sql, values);
-            returnRows = result.rowCount;
-        } catch (error) {
-            console.log(error)
-        }
-        return returnRows;
-    }
+    //  updateAsync = async (entity) => {
+    //     let returnRows = null;
+    //      try {
+    //         let id = entity.id;
+    //         const previousEntity = await this.getByIdAsync(id);
+    //         if (previousEntity == null) return 0;
+    //         const sql = `UPDATE provincias SET
+    //                     name              = $2,
+    //                     latitude            = $3,
+    //                     longitude            = $4,
+    //                     fullname    = $5,
+    //                     displayorder       = $6
+    //                 WHERE id = $1`;
+    //         const values = [
+    //         id,
+    //         entity?.name           ?? previousEntity?.name,
+    //         entity?.latitude         ?? previousEntity?.latitude,
+    //         entity?.longitude         ?? previousEntity?.longitude,
+    //         entity?.fullname ?? previousEntity?.fullname,
+    //         entity?.displayorder    ?? previousEntity?.displayorder
+    //     ];
+    //         const result = await this.getDBPool().query(sql, values);
+    //         returnRows = result.rowCount;
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
+    //     return returnRows;
+    // }
 
 
     createAsync = async (entity) => {
@@ -89,23 +89,17 @@ export default class publicacionRepository {
 
         try {
             const sql = ` INSERT INTO provincias (
-                            name              , 
-                            latitude            , 
-                            longitude            , 
-                            fullname    , 
-                            displayorder
+                            usuarioID              , 
+                            urlImagen           , 
+                            descripcion            
                         ) VALUES (
                             $1, 
                             $2, 
-                            $3, 
-                            $4, 
-                            $5
+                            $3
                         ) RETURNING id`;
-            const values =  [   entity.name,
-                                entity.latitude,
-                                entity.longitude,
-                                entity.fullname,
-                                entity.displayorder
+            const values =  [   entity.usuarioID,
+                                entity.urlImagen,
+                                entity.descripcion
                             ];
             const resultPg = await this.getDBPool().query(sql, values);
             newId = resultPg.rows[0].id;
